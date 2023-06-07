118b54fa91b1a208af92837149900e6f6eb23f80
Mirko Lenz <mirko@mirkolenz.com>
2023-06-08 00:45:38 +0200

    feat: initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+# gRPC-Web/Connect Proxy
+
+This is a gRPC-Web/Connect proxy server which translates gRPC-Web or Connect requests into regular gRPC HTTP/2 requests via envoy.
+
+## Usage
+
+```shell
+docker run --rm -it -p 50052:50052 -e PROXY_LISTEN_PORT=50052 -e PROXY_BACKEND_PORT=50051 ghcr.io/recap-utr/grpc-proxy:latest
+```
+
+## Parameters
+
+| Parameter               | Description                                                           | Default                |
+| ----------------------- | --------------------------------------------------------------------- | ---------------------- |
+| `PROXY_LISTEN_PORT`     | The port at which the proxy server will listen for incoming requests. | _required_             |
+| `PROXY_BACKEND_PORT`    | The port of the gRPC backend server.                                  | _required_             |
+| `PROXY_BACKEND_HOST`    | The host of the gRPC backend server.                                  | `host.docker.internal` |
+| `PROXY_BACKEND_TIMEOUT` | Time to wait for a response from the backend server.                  | `120s`                 |
+| `PROXY_ACCESS_LOG`      | The path to envoy's access log file                                   | `/dev/stdout`          |
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        pkgs,
+        system,
+        lib,
+        self',
+        ...
+      }: {
+        apps = let
+          app = {
+            type = "app";
+            program = lib.getExe self'.packages.default;
+          };
+        in {
+          copyDockerImage = {
+            type = "app";
+            program = builtins.toString (pkgs.writeShellScript "copyDockerImage" ''
+              IFS=$'\n' # iterate over newlines
+              set -x # echo on
+              for DOCKER_TAG in $DOCKER_METADATA_OUTPUT_TAGS; do
+                ${lib.getExe pkgs.skopeo} --insecure-policy copy "docker-archive:${self'.packages.dockerImage}" "docker://$DOCKER_TAG"
+              done
+            '');
+          };
+          default = app;
+        };
+        packages = let
+          entrypoint = pkgs.writeShellApplication {
+            name = "entrypoint";
+            text = ''
+              PARSED_ENVOY_CONFIG=$(${pkgs.toybox}/bin/mktemp -d)/envoy.yaml
+              ${lib.getExe pkgs.envsubst} -no-unset -no-empty -i "${./envoy.yaml}" -o "$PARSED_ENVOY_CONFIG"
+              ${lib.getExe pkgs.envoy} -c "$PARSED_ENVOY_CONFIG"
+            '';
+          };
+        in {
+          grpc-proxy = entrypoint;
+          default = entrypoint;
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "grpc-proxy";
+            tag = "latest";
+            created = "now";
+            runAsRoot = ''
+              #!${pkgs.runtimeShell}
+              mkdir -p tmp
+            '';
+            config = {
+              entrypoint = [(lib.getExe entrypoint)];
+              cmd = [];
+            };
+          };
+        };
+      };
+    };
+}
