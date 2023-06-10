3799991effc3c7c116b77c9e1eccdbea31e487ca
Mirko Lenz <mirko@mirkolenz.com>
2023-06-11 00:46:18 +0200

    refactor: use coretools instead of toybox

--- flake.nix
+++ flake.nix
@@ -28,14 +28,15 @@
             name = "entrypoint";
             text = ''
               if [ -z "''${PROXY_PORT:-}" ] || [ -z "''${BACKEND_PORT:-}" ]; then
-                ${pkgs.toybox}/bin/echo "The following parameters are required: PROXY_PORT, BACKEND_PORT." >&2
-                ${pkgs.toybox}/bin/echo "Open the manual for details: https://github.com/mirkolenz/grpc-proxy" >&2
+                ${pkgs.coreutils}/bin/echo "The following parameters are required: PROXY_PORT, BACKEND_PORT." >&2
+                ${pkgs.coreutils}/bin/echo "Open the manual for details: https://github.com/mirkolenz/grpc-proxy" >&2
                 exit 1
               fi
-              PROXY_OUTDIR=$(${pkgs.toybox}/bin/mktemp -d)
+
+              PROXY_OUTDIR=$(${pkgs.coreutils}/bin/mktemp --directory)
               PROXY_ENVOY_CONFIG="$PROXY_OUTDIR/envoy.yaml"
 
-              ${pkgs.toybox}/bin/env -i \
+              ${pkgs.coreutils}/bin/env --ignore-environment \
                 ${builtins.toString (lib.mapAttrsToList (key: val: "${key}=${val}") env)} \
                 "$@" \
                 ${lib.getExe pkgs.gomplate} \
