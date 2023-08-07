e9b50ea409650c0565a337d8ff49a3cdeea8aaf1
Mirko Lenz <mirko@mirkolenz.com>
2023-08-07 13:10:03 +0200

    fix: print generated envoy config on startup

--- default.nix
+++ default.nix
@@ -27,6 +27,9 @@ writeShellApplication {
       --file ${./envoy.yaml} \
       --out "$PROXY_ENVOY_CONFIG"
 
+    ${coreutils}/bin/echo "Starting Envoy with the following configuration:"
+    ${coreutils}/bin/cat "$PROXY_ENVOY_CONFIG"
+
     ${lib.getExe envoy} -c "$PROXY_ENVOY_CONFIG"
   '';
 }
