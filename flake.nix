c10f2b7a9841016c9fc188bbebf028e5d66acac8
Mirko Lenz <mirko@mirkolenz.com>
2024-04-22 13:35:35 +0200

    chore: use nixos 23.11 due to envoy issue

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     flocken = {
@@ -12,53 +12,64 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-    systems,
-    flocken,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      flocken,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = {
-        pkgs,
-        system,
-        lib,
-        self',
-        ...
-      }: {
-        _module.args = {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.gomod2nix.overlays.default
+      perSystem =
+        {
+          pkgs,
+          system,
+          lib,
+          self',
+          ...
+        }:
+        {
+          _module.args = {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ inputs.gomod2nix.overlays.default ];
+            };
+          };
+          packages = {
+            default = pkgs.callPackage ./. { };
+            grpcProxy = self'.packages.default;
+            full = pkgs.callPackage ./full.nix { };
+            docker = pkgs.callPackage ./docker.nix { };
+            releaseEnv = pkgs.buildEnv {
+              name = "releaseEnv";
+              paths = with pkgs; [
+                go
+                goreleaser
+                gomod2nix
+              ];
+            };
+            gomod2nix = pkgs.gomod2nix;
+          };
+          legacyPackages.dockerManifest = flocken.legacyPackages.${system}.mkDockerManifest {
+            github = {
+              enable = true;
+              token = "$GH_TOKEN";
+            };
+            version = builtins.getEnv "VERSION";
+            images = with self.packages; [
+              x86_64-linux.docker
+              aarch64-linux.docker
+            ];
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              goreleaser
             ];
           };
         };
-        packages = {
-          default = pkgs.callPackage ./. {};
-          grpcProxy = self'.packages.default;
-          full = pkgs.callPackage ./full.nix {};
-          docker = pkgs.callPackage ./docker.nix {};
-          releaseEnv = pkgs.buildEnv {
-            name = "releaseEnv";
-            paths = with pkgs; [go goreleaser gomod2nix];
-          };
-          gomod2nix = pkgs.gomod2nix;
-        };
-        legacyPackages.dockerManifest = flocken.legacyPackages.${system}.mkDockerManifest {
-          github = {
-            enable = true;
-            token = "$GH_TOKEN";
-          };
-          version = builtins.getEnv "VERSION";
-          images = with self.packages; [x86_64-linux.docker aarch64-linux.docker];
-        };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [go goreleaser];
-        };
-      };
     };
 }
