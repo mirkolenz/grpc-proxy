9d72694c507bf8527844f76ddc58acc740d50c1b
Mirko Lenz <mirko@mirkolenz.com>
2024-10-19 17:16:37 +0200

    chore(nix): add metadata to package

--- default.nix
+++ default.nix
@@ -1,6 +1,14 @@
-{ buildGoApplication }:
+{ buildGoApplication, lib }:
 buildGoApplication {
   name = "grpc-proxy";
   pwd = ./.;
   src = ./.;
+  meta = with lib; {
+    description = "Proxy server built on envoy providing a REST gateway and the ability to translate gRPC-Web and/or Connect requests requests into regular gRPC requests.";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mirkolenz ];
+    platforms = with platforms; linux ++ darwin;
+    homepage = "https://github.com/mirkolenz/grpc-proxy";
+    mainProgram = "grpc-proxy";
+  };
 }
